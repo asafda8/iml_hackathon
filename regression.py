3ec79dbf4d4f579611d97fdff8090e3ad40e140a
asaf dalal <asaf.dallal@mail.huji.ac.il>
2021-06-02 21:32:57 +0300

    delete split from main file

--- regression.py
+++ regression.py
@@ -17,18 +17,3 @@ def predict(csv_file):
     #your code goes here...
 
     pass
-
-
-def split_data():
-    file_path = 'C:\\Users\\asafd\\PycharmProjects\\iml_hackathon\\movies_dataset.csv'
-    df = pd.read_csv(file_path)
-    train = df.sample(frac = 0.6)
-    indices = train.index
-    new_df = df.loc[~df.index.isin(indices)]
-    validation = new_df.sample(frac=0.5)
-    test = new_df.loc[~new_df.index.isin(validation.index)]
-    train.to_csv('train.csv')
-    validation.to_csv('validation.csv')
-    test.to_csv('test.csv')
-
-split_data()
